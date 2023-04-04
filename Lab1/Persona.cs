c705760d97409a6a7cdf9e6a827e1af1b4048c81
Valeriano <daniel.valeriano@tecsup.edu.pe>
2023-04-04 08:36:41 -0500

    primer commit

---
+++ Lab1/Persona.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Persona : Form
+    {
+        SqlConnection conn;
+
+        public Persona(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+        private void Persona_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    String sql = "SELECT * FROM tbl_usuario";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dgvListado.DataSource = dt;
+                    dgvListado.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("La conexion esta cerrada");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del servidor: \n" +
+                    ex.ToString());
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String nombre = txtNombre.Text;
+                if (conn.State == ConnectionState.Open)
+                {
+                    String sql = "SELECT * FROM tbl_usuario WHERE usuario_nombre = '" + nombre + "';";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dgvListado.DataSource = dt;
+                    dgvListado.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("La conexion esta cerrada");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del servidor: \n" +
+                    ex.ToString());
+            }
+        }
+    }
+}
